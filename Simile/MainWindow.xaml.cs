d6fa2133b8c0592c42474769ccb914b93fcc0450
Goipiot <goipiot@gmail.com>
2017-05-21 18:46:52 +0300

    Docviewer class, new MainWindow Design

--- Simile/MainWindow.xaml.cs
+++ Simile/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
 
 namespace Simile
 {
@@ -24,6 +26,34 @@ namespace Simile
         {
             InitializeComponent();
 
+        }
+
+        private void Addbutton1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void InfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile1 = new OpenFileDialog();
+            if(openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = openFile1.FileName;
+                string outfile = "E\\file.pdf";
+                string extension = System.IO.Path.GetExtension(file);
+                if(extension == ".doc"|| extension == ".docx")
+                {
+                    Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+                    Document doc = null;
+                    doc = app.Documents.Open(file, Type.Missing, false);
+
+                    doc.ExportAsFixedFormat(outfile, WdExportFormat.wdExportFormatPDF);
+                    doc.Close(false, Type.Missing, Type.Missing);
+                    app.Quit(false, false, false);
+                    System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+
+                }
+                AxAcroPDF1.scr = outfile;
+            }
         }
     }
 }
--- Simile/User.cs
+++ Simile/User.cs
@@ -8,5 +8,44 @@ namespace Simile
 {
     class User
     {
+        public const int DefaultRating = 5;
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // 0 - 10
+        private int _rating;
+
+        public int Rating
+        {
+            get { return _rating; }
+            set { _rating = value; }
+        }
+
+        public string Info
+        {
+            get
+            {
+                return $"{_name} - {_rating}";
+            }
+        }
+
+
+        public Lecturer(string name, int rating)
+        {
+            _name = name;
+            _rating = rating;
+        }
+
+        public Lecturer(string name) 
+            : this(name, DefaultRating) {
+        }
     }
+
+}
 }
